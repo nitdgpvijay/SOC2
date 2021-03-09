76ffac098a1849638895273a4255dc4a1074241f
vijay.kumar <vijay.kumar@nexusgroup.com>
2021-03-09 10:16:49 +0530

    Eureka server.

---
+++ netflix-eureka/src/test/java/com/banking/fake/service/netflixeureka/NetflixEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.fake.service.netflixeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
